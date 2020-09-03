a805735e0e3e097771087b25c7302608b4a065e8
keisuke.koshikawa <keisuke.koshikawa@medcare.jp>
2020-09-03 18:37:06 +0900

    initializing devise_token_auth

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create!(
+  email: 'test-user+1@medcare.jp',
+  password: 'password',
+  name: 'test-user',
+  nickname: 'test'
+)
